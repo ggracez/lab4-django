39f2e0de5056c99ca22177c0c56e1837733e55bd
ggracez <gracezhu13@gmail.com>
2023-01-30 18:55:44 -0700

    finished 1-4 in tutorial

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
